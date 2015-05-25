e8b612804e7a55387d727ccc19c1162d597c7292
vinsensiusvey <vinsensiusvey@gmail.com>
2015-05-25 21:10:17 +0700

    backend

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=127.0.0.1:3306;dbname=kartu',
+    'dsn' => 'mysql:host=localhost;dbname=kartu',
     'username' => 'root',
-    'password' => 'admin123',
+    'password' => 'vn20092010',
     'charset' => 'utf8',
 ];
