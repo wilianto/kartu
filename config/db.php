9b78daad78398da86046c708e0cf37daa65b86fe
wilianto <indrawan.wilianto@gmail.com>
2015-07-18 11:35:38 +0700

    hanya db

--- config/db.php
+++ config/db.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=127.0.0.1:3306;dbname=kartu',
     'username' => 'root',
-    'password' => 'vn20092010',
+    'password' => 'admin123',
     'charset' => 'utf8',
 ];
